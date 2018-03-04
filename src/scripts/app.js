ccacfc7e53a352c3fabd0bbbb68f42fc4ac321b1
simmons <371248720@qq.com>
2018-03-04 12:49:50 +0800

    '第一次提交代码到本地仓库'

---
+++ src/scripts/app.js
@@ -0,0 +1,11 @@
+/*
+* @Author: simmons
+* @Date:   2018-03-03 20:23:18
+* @Last Modified by:   simmons
+* @Last Modified time: 2018-03-03 23:53:30
+*/
+'use strict';
+
+(function() {
+  alert('23333333333');
+})();
---
+++ src/scripts/demo.js
@@ -0,0 +1,9 @@
+/*
+* @Author: simmons
+* @Date:   2018-03-03 22:46:40
+* @Last Modified by:   simmons
+* @Last Modified time: 2018-03-03 22:46:55
+*/
+'use strict';
+
+console.log('sdfs');
